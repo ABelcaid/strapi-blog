5fbfdb207927a57223fbe5b3ecc61e2a4edcd477
belcaid abderrahim <belcaidna@gmail.com>
2021-05-04 11:35:43 +0000

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '94cf289923e8543e17a4f3bef80cc041'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5963407a-d242-4ec2-bc39-304c8e976762'
+};
